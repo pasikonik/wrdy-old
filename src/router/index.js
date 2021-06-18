71bc73d5c30d180b7f95d28b05696e9679296a59
Michał <blind.x6@gmail.com>
2021-06-18 20:25:41 +0200

    Initial vue + basic deps + simple config

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '/@/components/HelloWorld.vue'
+// import Home from '/@/views/Home.vue'
+// import Login from '/@/views/Login.vue'
+// import Registration from '/@/views/Registration.vue'
+// import Lists from '/@/views/Lists.vue'
+// import List from '/@/views/List.vue'
+
+const ifNotAuthenticated = (_to, _from, next) => {
+  if (!store.getters.loggedIn) {
+    next()
+    return
+  }
+  next('/')
+}
+
+const ifAuthenticated = (_to, _from, next) => {
+  if (store.getters.loggedIn) {
+    next()
+    return
+  }
+  next('/login')
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HelloWorld,
+    beforeEnter: ifNotAuthenticated,
+  },
+  // {
+  //   path: '/login',
+  //   name: 'login',
+  //   component: Login,
+  //   beforeEnter: ifNotAuthenticated,
+  // },
+  // {
+  //   path: '/registration',
+  //   name: 'registration',
+  //   component: Registration,
+  // },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
